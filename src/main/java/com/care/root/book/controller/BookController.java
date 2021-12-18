2c9a4ed05a4351dd803622742dbfd08accb832cd
juyeong <bn0406@naver.com>
2021-12-18 15:14:16 +0900

    좌석 추가

--- src/main/java/com/care/root/book/controller/BookController.java
+++ src/main/java/com/care/root/book/controller/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.care.root.book.service.BookService;
 
@@ -15,5 +16,19 @@ public class BookController {
 		
 		
 		return "book/bookMain";
+	}
+	
+	@GetMapping("/seat")
+	public String seat(Model model,
+			@RequestParam String i,
+			@RequestParam String title,
+			@RequestParam String time,
+			@RequestParam String theater) {
+		
+		model.addAttribute("i", i);
+		model.addAttribute("title", title);
+		model.addAttribute("time", time);
+		model.addAttribute("theater",theater);
+		return "book/seat";
 	}
 }
