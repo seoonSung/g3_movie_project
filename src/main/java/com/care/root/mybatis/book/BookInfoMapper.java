9805aecab988dc42f8d271b88af9d630f7db2788
seoon <seoon1988@gmail.com>
2022-01-12 13:12:20 +0900

    잔디심기

--- src/main/java/com/care/root/mybatis/book/BookInfoMapper.java
+++ src/main/java/com/care/root/mybatis/book/BookInfoMapper.java
@@ -7,8 +7,6 @@ import org.apache.ibatis.annotations.Param;
 
 import com.care.root.book.dto.BookInfoDTO;
 
-
-
 public interface BookInfoMapper {
 	public void payment(@Param("days") String days, @Param("title") String title,
 						@Param("times") String times, @Param("theater") int theater, 
